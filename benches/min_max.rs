9ac3926ea314ce5ab7ec3e878351d1ece7b350d2
Ryan James Spencer <spencer.ryanjames@gmail.com>
2021-11-19 20:29:21 +1100

    Lift SIMD code up from i16 to i32

--- benches/min_max.rs
+++ benches/min_max.rs
@@ -3,14 +3,14 @@ use min_max_throughput::*;
 use std::mem::MaybeUninit;
 
 pub fn criterion_benchmark(c: &mut Criterion) {
-    let xs: Vec<_> = [MaybeUninit::<i64>::uninit(); 100]
+    let xs: Vec<_> = [MaybeUninit::<i64>::uninit(); 1000]
         .into_iter()
         .map(|mut uninit| {
             uninit.write(rand::random());
             unsafe { uninit.assume_init() }
         })
         .collect();
-    let ys: Vec<_> = [MaybeUninit::<i16>::uninit(); 100]
+    let ys: Vec<_> = [MaybeUninit::<i32>::uninit(); 1000]
         .into_iter()
         .map(|mut uninit| {
             uninit.write(rand::random());
@@ -30,8 +30,8 @@ pub fn criterion_benchmark(c: &mut Criterion) {
     c.bench_function("min_max_bitwise_02", |b| {
         b.iter(|| min_max_bitwise_02(black_box(&xs)))
     });
-    c.bench_function("min_max_simd_i16", |b| {
-        b.iter(|| unsafe { min_max_simd_i16(black_box(&ys)) })
+    c.bench_function("min_max_simd_i32", |b| {
+        b.iter(|| unsafe { min_max_simd_i32(black_box(&ys)) })
     });
 }
 
--- src/lib.rs
+++ src/lib.rs
@@ -46,32 +46,32 @@ pub fn pp_sse2_register(x: core::arch::x86_64::__m128i) -> core::arch::x86_64::_
 }
 
 #[cfg(target_arch = "x86_64")]
-pub unsafe fn min_max_simd_i16(buff: &[i16]) -> Option<(i16, i16)> {
+pub unsafe fn min_max_simd_i32(buff: &[i32]) -> Option<(i32, i32)> {
     // need to consider
     // * excess padding on non multiples of 8, e.g. 9 elements
     // * negative value
-    // * upgrading to i64 buffer, rather than i16
+    // * upgrading to i32 buffer, rather than i32
     use core::arch::x86_64::*;
-    unsafe fn store_to_mm_128i(
-        x7: i16,
-        x6: i16,
-        x5: i16,
-        x4: i16,
-        x3: i16,
-        x2: i16,
-        x1: i16,
-        x0: i16,
-    ) -> __m128i {
-        _mm_set_epi16(x0, x1, x2, x3, x4, x5, x6, x7)
+    unsafe fn store_to_mm_256i(
+        x7: i32,
+        x6: i32,
+        x5: i32,
+        x4: i32,
+        x3: i32,
+        x2: i32,
+        x1: i32,
+        x0: i32,
+    ) -> __m256i {
+        _mm256_set_epi32(x0, x1, x2, x3, x4, x5, x6, x7)
     }
-    let mut maxmax = _mm_setzero_si128();
-    let mut minmin = _mm_setzero_si128();
+    let mut maxmax = _mm256_setzero_si256();
+    let mut minmin = _mm256_setzero_si256();
     let mut max = *buff.get(0)?;
     let mut min = *buff.get(0)?;
-    let f8: &[__m128i] = &(0..(buff.len() / 8) + 1)
+    let f8: &[__m256i] = &(0..(buff.len() / 8) + 1)
         .into_iter()
         .map(|ix| {
-            store_to_mm_128i(
+            store_to_mm_256i(
                 *buff.get(ix * 8).unwrap_or(&0),
                 *buff.get(ix * 8 + 1).unwrap_or(&0),
                 *buff.get(ix * 8 + 2).unwrap_or(&0),
@@ -83,34 +83,36 @@ pub unsafe fn min_max_simd_i16(buff: &[i16]) -> Option<(i16, i16)> {
             )
         })
         .collect::<Vec<_>>();
-    let mut maxval = _mm_setzero_si128();
-    let mut minval = _mm_setzero_si128();
+    let mut maxval = _mm256_setzero_si256();
+    let mut minval = _mm256_setzero_si256();
     for chunk in f8 {
-        maxval = _mm_max_epi16(maxval, *chunk);
-        minval = _mm_min_epi16(minval, *chunk);
+        maxval = _mm256_max_epi32(maxval, *chunk);
+        minval = _mm256_min_epi32(minval, *chunk);
     }
+    // NOTE: if we go from i32 -> i64, we'll need to keep using __mm256_max_epi32.
+    // in which the case the below might matter.
+    // FIXME: I don't think the below actually does anything yet.
     //let mut maxval2 = maxval;
     //let mut minval2 = minval;
-    // FIXME: I don't think the below actually does anything yet.
     //for _ in 0..3 {
     //    //pp(maxval);
-    //    //pp(_mm_shufflehi_epi16::<3>(maxval));
-    //    //pp(_mm_shufflehi_epi16::<8>(maxval));
+    //    //pp(_mm_shufflehi_epi32::<3>(maxval));
+    //    //pp(_mm_shufflehi_epi32::<8>(maxval));
 
-    //    maxval = _mm_max_epi16(maxval, _mm_shufflehi_epi16::<3>(maxval));
-    //    _mm_store_si128(&mut maxmax as *mut __m128i, maxval);
-    //    maxval2 = _mm_max_epi16(maxval2, _mm_shufflelo_epi16::<3>(maxval2));
-    //    _mm_store_si128(&mut maxmax as *mut __m128i, maxval2);
+    //    maxval = _mm_max_epi32(maxval, _mm_shufflehi_epi32::<3>(maxval));
+    //    _mm_store_si256(&mut maxmax as *mut __m256i, maxval);
+    //    maxval2 = _mm_max_epi32(maxval2, _mm_shufflelo_epi32::<3>(maxval2));
+    //    _mm_store_si256(&mut maxmax as *mut __m256i, maxval2);
 
-    //    minval = _mm_min_epi16(minval, _mm_shufflehi_epi16::<3>(minval));
-    //    _mm_store_si128(&mut minmin as *mut __m128i, minval);
-    //    minval2 = _mm_min_epi16(minval2, _mm_shufflelo_epi16::<3>(minval2));
-    //    _mm_store_si128(&mut minmin as *mut __m128i, minval2);
+    //    minval = _mm_min_epi32(minval, _mm_shufflehi_epi32::<3>(minval));
+    //    _mm_store_si256(&mut minmin as *mut __m256i, minval);
+    //    minval2 = _mm_min_epi32(minval2, _mm_shufflelo_epi32::<3>(minval2));
+    //    _mm_store_si256(&mut minmin as *mut __m256i, minval2);
     //}
-    _mm_store_si128(&mut maxmax as *mut __m128i, maxval);
-    _mm_store_si128(&mut minmin as *mut __m128i, minval);
-    let maxmax: [i16; 8] = std::mem::transmute(maxmax);
-    let minmin: [i16; 8] = std::mem::transmute(minmin);
+    _mm256_store_si256(&mut maxmax as *mut __m256i, maxval);
+    _mm256_store_si256(&mut minmin as *mut __m256i, minval);
+    let maxmax: [i32; 8] = std::mem::transmute(maxmax);
+    let minmin: [i32; 8] = std::mem::transmute(minmin);
     // get_unchecked
     for i in 0..8 {
         if max < *maxmax.get_unchecked(i) {
@@ -136,12 +138,12 @@ mod test {
         result
     }
 
-    fn array_i16(size: usize) -> Vec<i16> {
+    fn array_i32(size: usize) -> Vec<i32> {
         let mut rng = thread_rng();
         let mut result: Vec<_> = std::iter::repeat(0)
             .take(size)
             .enumerate()
-            .map(|(i, _)| i as i16 + 1)
+            .map(|(i, _)| i as i32 + 1)
             .collect();
         result.shuffle(&mut rng);
         result
@@ -172,29 +174,29 @@ mod test {
     }
 
     #[test]
-    fn min_max_simd_i16_work_with_exact_sized_arrays() {
+    fn min_max_simd_i32_work_with_exact_sized_arrays() {
         unsafe {
-            assert_eq!(min_max_simd_i16(&array_i16(0)), None);
-            assert_eq!(min_max_simd_i16(&array_i16(8)), Some((0, 8)));
-            assert_eq!(min_max_simd_i16(&array_i16(16)), Some((0, 16)));
+            assert_eq!(min_max_simd_i32(&array_i32(0)), None);
+            assert_eq!(min_max_simd_i32(&array_i32(8)), Some((0, 8)));
+            assert_eq!(min_max_simd_i32(&array_i32(16)), Some((0, 16)));
         }
     }
 
     #[test]
-    fn min_max_simd_i16_work_with_inexact_sized_arrays() {
+    fn min_max_simd_i32_work_with_inexact_sized_arrays() {
         unsafe {
-            assert_eq!(min_max_simd_i16(&array_i16(9)), Some((0, 9)));
-            assert_eq!(min_max_simd_i16(&array_i16(10)), Some((0, 10)));
+            assert_eq!(min_max_simd_i32(&array_i32(9)), Some((0, 9)));
+            assert_eq!(min_max_simd_i32(&array_i32(10)), Some((0, 10)));
         }
     }
 
     #[test]
-    fn min_max_simd_i16_works() {
+    fn min_max_simd_i32_works() {
         let mut rng = thread_rng();
         for _ in 0..100 {
-            let size = rng.gen_range(0..i16::MAX);
+            let size = rng.gen_range(0..i32::MAX);
             unsafe {
-                assert_eq!(min_max_simd_i16(&array_i16(size as usize)), Some((0, size)));
+                assert_eq!(min_max_simd_i32(&array_i32(size as usize)), Some((0, size)));
             }
         }
     }
