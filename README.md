4acd16824ef5693e51fe4718dfd467fa7078a83e
Ryan James Spencer <spencer.ryanjames@gmail.com>
2021-11-21 12:12:51 +1100

    Speed up indiect SIMD implementation

--- README.md
+++ README.md
@@ -22,11 +22,11 @@ $ cargo criterion
 ### Results
 
 ```
-min_max_multiple_passes   time:   [626.15 ns 627.87 ns 630.08 ns]
-min_max_conditional       time:   [182.17 ns 182.56 ns 183.04 ns]
-min_max_bitwise_01        time:   [182.14 ns 182.40 ns 182.70 ns]
-min_max_bitwise_02        time:   [1.3124 us 1.3134 us 1.3150 us]
-min_max_simd_i32_direct   time:   [160.03 ns 160.80 ns 161.78 ns]
-min_max_simd_i32_indirect time:   [1.7274 us 1.7298 us 1.7325 us]
-min_max_portable_simd     time:   [2.1274 us 2.1408 us 2.1595 us]
+min_max_multiple_passes    time:   [620.82 ns 628.99 ns 638.34 ns]
+min_max_conditional        time:   [183.14 ns 184.40 ns 186.02 ns]
+min_max_bitwise_01         time:   [182.30 ns 182.84 ns 183.59 ns]
+min_max_bitwise_02         time:   [1.3158 us 1.3180 us 1.3208 us]
+min_max_simd_i32_direct    time:   [161.92 ns 162.14 ns 162.37 ns]
+min_max_simd_i32_indirect  time:   [418.78 ns 421.09 ns 424.25 ns]
+min_max_portable_simd      time:   [2.0696 us 2.0820 us 2.0965 us]
 ```
--- src/lib.rs
+++ src/lib.rs
@@ -20,13 +20,20 @@ pub fn min_max_conditional(integers: &[i64]) -> Option<(i64, i64)> {
     })
 }
 
+#[inline(always)]
+pub fn min_bitwise(x: i64, y: i64) -> i64 {
+    y ^ ((x ^ y) & -((x < y) as i64))
+}
+
+#[inline(always)]
+pub fn max_bitwise(x: i64, y: i64) -> i64 {
+    x ^ ((x ^ y) & -((x < y) as i64))
+}
+
 pub fn min_max_bitwise_01(integers: &[i64]) -> Option<(i64, i64)> {
     integers.into_iter().fold(None, |acc, x| match acc {
         None => Some((*x, *x)),
-        Some((min, max)) => Some((
-            min ^ ((*x ^ min) & -((*x < min) as i64)),
-            *x ^ ((*x ^ max) & -((*x < max) as i64)),
-        )),
+        Some((min, max)) => Some((min_bitwise(*x, min), max_bitwise(*x, max))),
     })
 }
 
@@ -153,6 +160,7 @@ pub unsafe fn min_max_simd_i32_direct(buff: &[i32]) -> Option<(i32, i32)> {
     });
     let maxmax: [i32; 8] = std::mem::transmute(maxval);
     let minmin: [i32; 8] = std::mem::transmute(minval);
+    // TODO: try min_bitwise and max_bitwise below.
     for i in 0..8 {
         if max < *maxmax.get_unchecked(i) {
             max = *maxmax.get_unchecked(i);
@@ -164,6 +172,42 @@ pub unsafe fn min_max_simd_i32_direct(buff: &[i32]) -> Option<(i32, i32)> {
     Some((min, max))
 }
 
+/// NOTE: This implements a more traditional aproach, although with 256-bit lanes.
+///
+/// 1. Take a single register
+/// 2. Shufflehi/shufflelo the elements rightwise
+/// 3. Calculate the pairwise reduction (min/max/sum/product/etc.)
+/// 4. Repeat at (1) until only a single element remains
+///
+/// Visualised:
+///
+/// (1.1) - initial
+/// ┌──────────────────┐
+/// │ 12 | 8 | -2 | 11 │ xmm0
+/// └──────────────────┘
+///
+/// (2.1) - shuffle
+/// ┌──────────────────┐
+/// │ XX | XX | 12 | 8 │ xmm1
+/// └──────────────────┘
+///
+/// (3.1) - max
+/// ┌──────────────────┐
+/// │ XX | X | 12 | 11 │ xmm2
+/// └──────────────────┘
+///
+/// (2.2) - shuffle
+/// ┌──────────────────┐
+/// │ XX | X | XX | 12 │ xmm1
+/// └──────────────────┘
+///
+/// (3.2) - max
+/// ┌──────────────────┐
+/// │ XX | X | XX | 12 │ xmm1
+/// └──────────────────┘
+///
+/// The point is we can consider the buildup to the left as trash and take advantage of the
+/// faster vertical instructions.
 #[cfg(target_arch = "x86_64")]
 pub unsafe fn min_max_simd_i32_indirect(buff: &[i32]) -> Option<(i32, i32)> {
     if buff.is_empty() {
@@ -171,96 +215,60 @@ pub unsafe fn min_max_simd_i32_indirect(buff: &[i32]) -> Option<(i32, i32)> {
     }
     let mut min = i32::MAX;
     let mut max = i32::MIN;
-    let f8: &[__m256i] = &buff
-        .chunks(8)
-        .into_iter()
-        .filter_map(|slice| {
-            if slice.len() == 8 {
-                Some(store_to_mm_256i(
-                    *slice.get_unchecked(0),
-                    *slice.get_unchecked(1),
-                    *slice.get_unchecked(2),
-                    *slice.get_unchecked(3),
-                    *slice.get_unchecked(4),
-                    *slice.get_unchecked(5),
-                    *slice.get_unchecked(6),
-                    *slice.get_unchecked(7),
-                ))
-            } else {
-                max = *slice.iter().max()?;
-                min = *slice.iter().min()?;
-                None
+    buff.chunks(8).into_iter().for_each(|slice| {
+        if slice.len() == 8 {
+            let x = store_to_mm_256i(
+                *slice.get_unchecked(0),
+                *slice.get_unchecked(1),
+                *slice.get_unchecked(2),
+                *slice.get_unchecked(3),
+                *slice.get_unchecked(4),
+                *slice.get_unchecked(5),
+                *slice.get_unchecked(6),
+                *slice.get_unchecked(7),
+            );
+
+            // TODO: try shift right instead of permutevar with control vector.
+            //let shuffled = _mm256_shuffle_epi32::<0b00_00_00_11>(x);
+            let indices = store_to_mm_256i(0, 0, 0, 0, 3, 2, 1, 0);
+            let shuffled = _mm256_permutevar8x32_epi32(x, indices);
+            let max1 = _mm256_max_epi32(shuffled, x);
+            let indices = store_to_mm_256i(0, 0, 0, 0, 0, 0, 5, 4);
+            let shuffled = _mm256_permutevar8x32_epi32(max1, indices);
+            let max2 = _mm256_max_epi32(shuffled, max1);
+            let indices = store_to_mm_256i(0, 0, 0, 0, 0, 0, 0, 6);
+            let shuffled = _mm256_permutevar8x32_epi32(max2, indices);
+            let max3 = _mm256_max_epi32(shuffled, max2);
+
+            let indices = store_to_mm_256i(0, 0, 0, 0, 3, 2, 1, 0);
+            let shuffled = _mm256_permutevar8x32_epi32(x, indices);
+            let min1 = _mm256_min_epi32(shuffled, x);
+            let indices = store_to_mm_256i(0, 0, 0, 0, 0, 0, 5, 4);
+            let shuffled = _mm256_permutevar8x32_epi32(min1, indices);
+            let min2 = _mm256_min_epi32(shuffled, min1);
+            let indices = store_to_mm_256i(0, 0, 0, 0, 0, 0, 0, 6);
+            let shuffled = _mm256_permutevar8x32_epi32(min2, indices);
+            let min3 = _mm256_min_epi32(shuffled, min2);
+
+            let local_max = _mm256_extract_epi32(max3, 7);
+            let local_min = _mm256_extract_epi32(min3, 7);
+            if local_max > max {
+                max = local_max;
             }
-        })
-        .collect::<Vec<_>>();
-    // NOTE: This implements a more traditional aproach, although with 256-bit lanes.
-    //
-    // 1. Take a single register
-    // 2. Shufflehi/shufflelo the elements rightwise
-    // 3. Calculate the pairwise reduction (min/max/sum/product/etc.)
-    // 4. Repeat at (1) until only a single element remains
-    //
-    // Visualised:
-    //
-    // (1.1) - initial
-    // ┌──────────────────┐
-    // │ 12 | 8 | -2 | 11 │ xmm0
-    // └──────────────────┘
-    //
-    // (2.1) - shuffle
-    // ┌──────────────────┐
-    // │ XX | XX | 12 | 8 │ xmm1
-    // └──────────────────┘
-    //
-    // (3.1) - max
-    // ┌──────────────────┐
-    // │ XX | X | 12 | 11 │ xmm2
-    // └──────────────────┘
-    //
-    // (2.2) - shuffle
-    // ┌──────────────────┐
-    // │ XX | X | XX | 12 │ xmm1
-    // └──────────────────┘
-    //
-    // (3.2) - max
-    // ┌──────────────────┐
-    // │ XX | X | XX | 12 │ xmm1
-    // └──────────────────┘
-    //
-    // The point is we can consider the buildup to the left as trash and take advantage of the
-    // faster vertical instructions.
-
-    for x in f8 {
-        //let shuffled = _mm256_shuffle_epi32::<0b00_00_00_11>(x);
-        let indices = store_to_mm_256i(0, 0, 0, 0, 3, 2, 1, 0);
-        let shuffled = _mm256_permutevar8x32_epi32(*x, indices);
-        let max1 = _mm256_max_epi32(shuffled, *x);
-        let indices = store_to_mm_256i(0, 0, 0, 0, 0, 0, 5, 4);
-        let shuffled = _mm256_permutevar8x32_epi32(max1, indices);
-        let max2 = _mm256_max_epi32(shuffled, max1);
-        let indices = store_to_mm_256i(0, 0, 0, 0, 0, 0, 0, 6);
-        let shuffled = _mm256_permutevar8x32_epi32(max2, indices);
-        let max3 = _mm256_max_epi32(shuffled, max2);
-
-        let indices = store_to_mm_256i(0, 0, 0, 0, 3, 2, 1, 0);
-        let shuffled = _mm256_permutevar8x32_epi32(*x, indices);
-        let min1 = _mm256_min_epi32(shuffled, *x);
-        let indices = store_to_mm_256i(0, 0, 0, 0, 0, 0, 5, 4);
-        let shuffled = _mm256_permutevar8x32_epi32(min1, indices);
-        let min2 = _mm256_min_epi32(shuffled, min1);
-        let indices = store_to_mm_256i(0, 0, 0, 0, 0, 0, 0, 6);
-        let shuffled = _mm256_permutevar8x32_epi32(min2, indices);
-        let min3 = _mm256_min_epi32(shuffled, min2);
-
-        let local_max = _mm256_extract_epi32(max3, 7);
-        let local_min = _mm256_extract_epi32(min3, 7);
-        if local_max > max {
-            max = local_max;
+            if local_min < min {
+                min = local_min;
+            }
+        } else {
+            let local_max = *slice.iter().max().unwrap();
+            let local_min = *slice.iter().min().unwrap();
+            if local_max > max {
+                max = local_max;
+            }
+            if local_min < min {
+                min = local_min;
+            }
         }
-        if local_min < min {
-            min = local_min;
-        }
-    }
+    });
     Some((min, max))
 }
 
@@ -317,38 +325,75 @@ mod test {
     }
 
     #[test]
-    fn min_max_simd_i32_work_with_exact_sized_arrays() {
+    fn min_max_simd_i32_direct_work_with_exact_sized_arrays() {
         unsafe {
             assert_eq!(min_max_simd_i32(&array_i32(0)), None);
             for x in 1..3 {
                 let array = array_i32(x * 8);
                 let min = *array.iter().min().unwrap();
                 let max = *array.iter().max().unwrap();
-                assert_eq!(min_max_simd_i32(&array), Some((min, max)));
+                assert_eq!(min_max_simd_i32_direct(&array), Some((min, max)));
             }
         }
     }
 
     #[test]
-    fn min_max_simd_i32_work_with_inexact_sized_arrays() {
+    fn min_max_simd_i32_direct_work_with_inexact_sized_arrays() {
         unsafe {
             for x in 9..11 {
                 let array = array_i32(x);
                 let min = *array.iter().min().unwrap();
                 let max = *array.iter().max().unwrap();
-                assert_eq!(min_max_simd_i32(&array), Some((min, max)));
+                assert_eq!(min_max_simd_i32_direct(&array), Some((min, max)));
             }
         }
     }
 
     #[test]
-    fn min_max_simd_i32_works() {
+    fn min_max_simd_i32_direct_works() {
         for size in 1..100 + 1 {
             unsafe {
                 let array = array_i32(size);
                 let min = *array.iter().min().unwrap();
                 let max = *array.iter().max().unwrap();
-                assert_eq!(min_max_simd_i32(&array), Some((min, max)));
+                assert_eq!(min_max_simd_i32_direct(&array), Some((min, max)));
+            }
+        }
+    }
+
+    #[test]
+    fn min_max_simd_i32_indirect_work_with_exact_sized_arrays() {
+        unsafe {
+            assert_eq!(min_max_simd_i32(&array_i32(0)), None);
+            for x in 1..3 {
+                let array = array_i32(x * 8);
+                let min = *array.iter().min().unwrap();
+                let max = *array.iter().max().unwrap();
+                assert_eq!(min_max_simd_i32_indirect(&array), Some((min, max)));
+            }
+        }
+    }
+
+    #[test]
+    fn min_max_simd_i32_indirect_work_with_inexact_sized_arrays() {
+        unsafe {
+            for x in 9..11 {
+                let array = array_i32(x);
+                let min = *array.iter().min().unwrap();
+                let max = *array.iter().max().unwrap();
+                assert_eq!(min_max_simd_i32_indirect(&array), Some((min, max)));
+            }
+        }
+    }
+
+    #[test]
+    fn min_max_simd_i32_indirect_works() {
+        for size in 1..100 + 1 {
+            unsafe {
+                let array = array_i32(size);
+                let min = *array.iter().min().unwrap();
+                let max = *array.iter().max().unwrap();
+                assert_eq!(min_max_simd_i32_indirect(&array), Some((min, max)));
             }
         }
     }
